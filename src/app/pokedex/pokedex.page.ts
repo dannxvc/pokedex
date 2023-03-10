e8188d6d8fd29e7b86d629f4c6a50fb58f41e3fb
danna <dannavila.1809@gmail.com>
2023-03-10 13:38:28 -0500

    Create a catalogue and implement the pokeAPI

---
+++ src/app/pokedex/pokedex.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PokemonService } from '../service/pokemon.service';
+
+@Component({
+  selector: 'app-pokedex',
+  templateUrl: './pokedex.page.html',
+  styleUrls: ['./pokedex.page.scss'],
+})
+export class PokedexPage implements OnInit {
+
+  pokemons : any[] =[];
+
+
+  constructor(
+    private pokeService: PokemonService
+  ) {}
+
+  ngOnInit(): void {
+    this.getPokemons();
+  }
+  
+  getPokemons(){
+
+    let pokemonData;
+
+    for(let i = 1; i <=100; i++){
+      this.pokeService.getPokemons(i).subscribe(
+        res => {
+          pokemonData = {
+            position: i,
+            img: res.sprites.other.dream_world.front_default,
+            name: res.name
+          };
+          this.pokemons.push(pokemonData);
+          console.log(res);
+        },
+        err=>{
+  
+        }
+      )
+    }
+  }
+    
+}
